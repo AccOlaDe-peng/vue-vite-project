f385e908a67fa4a0315cffc05f99fe14b78e8361
彭仁常 <pengrch@activeio.com.cn>
2023-04-05 21:12:09 +0800

    fix: 初始化项目

---
+++ src/router/index.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import HelloWorld from "../components/HelloWorld.vue";
+
+const routes: RouteRecordRaw[] = [
+    { path: "/", component: HelloWorld },
+    { path: "/about", component: HelloWorld },
+];
+
+const router = createRouter({
+    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: createWebHashHistory(),
+    routes, // `routes: routes` 的缩写
+});
+
+router.beforeEach((to, from, next) => {
+    console.log(to, from);
+    document.title = `${to.meta.title} | vue-manage-system`;
+    // const role = localStorage.getItem("ms_username");
+    // if (!role && to.path !== "/login") {
+    //     next("/login");
+    // }
+    //用户权限
+    // else if (to.meta.permiss && !permiss.key.includes(to.meta.permiss)) {
+    //     // 如果没有权限，则进入403
+    //     next("/403");
+    // }
+    // else {
+    next();
+    // }
+});
+
+export default router;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+declare module "*.vue" {
+    import { defineComponent } from "vue";
+    const Component: DefineComponent<{}, {}, any>;
+    export default Component;
+}
