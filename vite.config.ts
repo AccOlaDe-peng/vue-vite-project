9106656fa79e07aac29c7ab34cd08cb8f892f33b
pengrenchang <pengrch@activeio.com.cn>
2023-12-09 20:50:34 +0800

    fix:  修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-// import VueSetupExtend from "vite-plugin-vue-setup-extend";
+import VueJSX from "@vitejs/plugin-vue-jsx";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { viteMockServe } from "vite-plugin-mock";
@@ -10,6 +10,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 export default defineConfig({
 	plugins: [
 		vue(),
+		VueJSX(),
 		AutoImport({
 			resolvers: [ElementPlusResolver()],
 		}),
@@ -28,6 +29,7 @@ export default defineConfig({
 		},
 	},
 	server: {
+		host: "0.0.0.0",
 		port: 3300,
 		hmr: {
 			overlay: false,
